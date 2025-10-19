a8aa3139d1656399808c8b139ac0e62c71f1e96f
hasankhalifa2001-glitch <hasan.khalifa2001@gmail.com>
2025-10-19 16:36:57 +0300

    Complate Navbar component and Guest, about and contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import Guest from "@/components/Guest"
+import { currentUser } from "@clerk/nextjs/server"
 
-const HomePage = () => {
+
+const HomePage = async () => {
+
+  const user = await currentUser()
+
+  if (!user) {
+    return <Guest />
+  }
+
   return (
     <div>HomePage</div>
   )
